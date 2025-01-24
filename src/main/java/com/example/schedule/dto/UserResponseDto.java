e5e10b08579c5ce9b4d30f8ed120917f6f6529db
yrchoi <daylikezero@gmail.com>
2025-01-24 15:21:37 +0900

    #2 refactor: response datetime format ISO_LOCAL_DATE

--- src/main/java/com/example/schedule/dto/UserResponseDto.java
+++ src/main/java/com/example/schedule/dto/UserResponseDto.java
@@ -1,26 +1,32 @@
 package com.example.schedule.dto;
 
 import com.example.schedule.entity.User;
-import lombok.AllArgsConstructor;
+import com.example.schedule.util.LocalDateTimeUtils;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class UserResponseDto {
     private Long id;
     private String name;
     private String email;
-    private LocalDateTime regDate;
-    private LocalDateTime modDate;
+    private String regDate;
+    private String modDate;
 
     public UserResponseDto(User user) {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
-        this.regDate = user.getRegDate();
-        this.modDate = user.getModDate();
+        this.regDate = LocalDateTimeUtils.formatToIsoLocalDate(user.getRegDate());
+        this.modDate = LocalDateTimeUtils.formatToIsoLocalDate(user.getModDate());
+    }
+
+    public UserResponseDto(Long id, String name, String email, LocalDateTime regDate, LocalDateTime modDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.regDate = LocalDateTimeUtils.formatToIsoLocalDate(regDate);
+        this.modDate = LocalDateTimeUtils.formatToIsoLocalDate(modDate);
     }
 }
